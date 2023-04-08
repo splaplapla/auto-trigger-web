490e6de393f7a107053933f34afdd537a5f66cb0
jiikko <n905i.1214@gmail.com>
2023-04-08 22:50:23 +0900

    aaaaaaaaaa

--- app/models/procon_bypass_man_command.rb
+++ app/models/procon_bypass_man_command.rb
@@ -1,4 +1,14 @@
 class ProconBypassManCommand < ApplicationRecord
   validates :name, :first_buttons, presence: true
   validates :repeat_count, numericality: { greater_than_or_equal_to: 1 }
+  # TODO: スペースは容認しつつ、カンマ区切りかつ、存在しないボタンをブロックするバリデーションを入れる
+
+  # TODO: カラムの中身を, でsplitしたほうがいいかも
+  def to_commands_as_remote
+    repeat_count.times.reduce([]){ |a, item|
+      a.push(first_buttons)
+      a.push(second_buttons) if second_buttons.present?
+      next a
+    }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 ProconBypassManHost.find_or_create_by!(name: 'ras1.local', port: 9000)
 ProconBypassManCommand.find_or_create_by!(name: '射撃ボタンを連打', first_buttons: 'zr', second_buttons: 'unzr', repeat_count: 3)
+ProconBypassManCommand.find_or_create_by!(name: '射撃ボタンを離す', first_buttons: 'unzr', repeat_count: 3)
