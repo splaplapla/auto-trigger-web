eb40cad270cad97c0a689635d67b9e594fe2fc2e
jiikko <n905i.1214@gmail.com>
2023-04-12 20:30:18 +0900

    a

--- app/models/commander.rb
+++ app/models/commander.rb
@@ -1,47 +1,49 @@
 class Commander
-  def run
-    loop do
-      if not Setting.instance.capture_enabled
-        sleep(1)
-        redo
-      end
+  def initialize
+    puts 'start Commander'
 
-      # TODO: load detect class
-      @capture_enabled = true
-      procon_bypass_man_host = Setting.instance.procon_bypass_man_host
-
-      video_file = OpenCV::cv::VideoCapture.new(0)
-      frame = OpenCV::cv::Mat.new
-      frame_rate = 90.0
-      frame_duration = 1.0 / frame_rate
-      puts "writing video as single images at #{frame_rate} FPS"
-      start_time = Time.now
-      width, height = Camera::TARGET_SIZE.split('x').map(&:to_i)
-
-      while video_file.read(frame) && @capture_enabled
-        elapsed_time = Time.now - start_time
-        if elapsed_time >= frame_duration
-          OpenCV::cv::resize(frame, frame, OpenCV::cv::Size.new(width, height))
-          start_time = Time.now
-        end
-
-        # TODO: 360pでできないか検討する
-        # TODO: 画像サイズを小さくしたら早くなるかも？
-        if ProconBypassManCommander::Splatoon3::EnemyTargetDetectorHDLiter.detect?(frame)
-          # TODO: trueの時にスクショを保存する
-          procon_bypass_man_host.client.send_command(buttons: ['unzr'])
-          puts 'true'
-        else
-          puts 'false'
-        end
-
-        # TODO: Threadで取得する
+    Thread.new do
+      loop do
         Rails.cache.fetch('cache capture_enabled', expires_in: 3.seconds) do
           if not Setting.instance.capture_enabled
             @capture_enabled = false
           end
         end
+        sleep(3)
       end
+    end
+  end
+
+  def run
+    loop do
+      if not Setting.instance.capture_enabled
+        sleep(2)
+        redo
+      end
+      Rails.logger.info '処理を開始します'
+
+      @capture_enabled = true
+      procon_bypass_man_host = Setting.instance.procon_bypass_man_host
+
+      video_file = OpenCV::cv::VideoCapture.new(0)
+      frame = OpenCV::cv::Mat.new
+      width, height = Camera::TARGET_SIZE.split('x').map(&:to_i)
+
+      while video_file.read(frame) && @capture_enabled
+        OpenCV::cv::resize(frame, frame, OpenCV::cv::Size.new(width, height))
+        # TODO: 360pでできないか検討する
+        # TODO: 画像サイズを小さくしたら早くなるかも？
+        if ProconBypassManCommander::Splatoon3::EnemyTargetDetectorHDLiter.detect?(frame) # TODO: load detect class
+          procon_bypass_man_host.client.send_command(buttons: ['unzr'])
+          time = Time.now
+          OpenCV::cv::imwrite "./tmp/detected_images/#{time.to_i}#{time.usec}.png", frame
+          puts 'true'
+        else
+          puts 'false'
+        end
+      end
+
+      Rails.logger.info  '一時停止しました'
     end
   end
 end
--- app/models/procon_bypass_man_tcp_client.rb
+++ app/models/procon_bypass_man_tcp_client.rb
@@ -9,10 +9,10 @@ class ProconBypassManTcpClient
   # @return [Boolean]
   def send_command(buttons: )
     message = { buttons: buttons }.to_json
-    Post.benchmark("ProconBypassManTcpClient#send_command with #{message}") do
+    Rails.benchmark("ProconBypassManTcpClient#send_command with #{message}") do
       @socket.write(message)
     end
-    # command_response = @socket.gets # PBMの設定でレスポンスを送らないようにしたので読み取らない
+    command_response = @socket.gets
     # @socket.close # 接続をキャッシュしているのでcloseしない
     true
   end
