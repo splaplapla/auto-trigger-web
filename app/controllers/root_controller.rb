cc2f9066eb7eb18a1baa1d35b7e5f0f0f975a60e
jiikko <n905i.1214@gmail.com>
2023-04-15 19:16:00 +0900

    command ruleを一旦消すか

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,19 +1,5 @@
 class RootController < ApplicationController
   def index
-    @command_conditions = {
-      spl3_detect_for_charger: { name: 'ブキ(チャージャー)を使っていて標的にあったら' },
-      spl3_detect_for_shooter: { name: 'ブキ(シューター)を使っていて標的にあったら' },
-    }
-    @command_conditions.each do |key, hash|
-      CommandRule.find_or_create_by!(condition_key: key, condition_name: hash[:name])
-    end
+    @setting = Setting.instance
   end
-
-  private
-
-  def setting
-    @setting ||= Setting.instance
-  end
-
-  helper_method :setting
 end
--- app/models/command_rule.rb
+++
@@ -1,3 +0,0 @@
-class CommandRule < ApplicationRecord
-  belongs_to :procon_bypass_man_command, optional: true
-end
